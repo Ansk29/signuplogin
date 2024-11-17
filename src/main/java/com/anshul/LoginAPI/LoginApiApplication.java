b6786b0b427f6e6620228accac444d95bfa9f8e5
unknown <anshul.kakirde@gmail.com>
2024-11-17 14:11:29 +0530

    update

--- src/main/java/com/anshul/LoginAPI/LoginApiApplication.java
+++ src/main/java/com/anshul/LoginAPI/LoginApiApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 
-@SpringBootApplication
 public class LoginApiApplication {
 
     public static void main(String[] args) {
